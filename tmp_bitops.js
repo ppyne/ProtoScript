7ab0a8ae3dfbf91c9fb9897072a293d1d529b583
Parker Pyne <parker.pyne.detective@gmail.com>
2026-01-28 00:19:12 +0100

    Cleaning main

--- tmp_bitops.js
+++
@@ -1,6 +0,0 @@
-var col = 4284250757;
-var r = col & 0xff;
-var g = (col >> 8) & 0xff;
-var b = (col >> 16) & 0xff;
-var a = (col >>> 24) & 0xff;
-Io.print("r=" + r + " g=" + g + " b=" + b + " a=" + a + "\n");
--- tmp_buf32_present.js
+++
@@ -1,1 +0,0 @@
-Io.print("Buffer32=" + typeof Buffer32 + " view=" + (Buffer32 ? typeof Buffer32.view : "na") + "\n");
